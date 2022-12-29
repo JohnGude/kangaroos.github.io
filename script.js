7ae5bebf2574296b2881297f6f1b2e33953fb5c1
JohnGude <S3973475@student.rmit.edu.au>
2022-12-29 13:47:46 +1100

    Initial template

---
+++ script.js
@@ -0,0 +1,67 @@
+import {kangaroo_team_profiles} from "./text.js";
+import {it_interests} from "./text.js";
+import {career_profiling} from "./text.js";
+import {it_technologies} from "./text.js";
+import {team_project} from "./text.js";
+import {meetings} from "./text.js";
+import {team_reflections} from "./text.js";
+import {contacts} from "./text.js";
+
+
+
+const pages = ["kangaroo_team_profiles","it_interests","career_profiling","it_technologies","team_project","meetings","team_reflections","contacts"]
+const imports = [kangaroo_team_profiles,it_interests,career_profiling,it_technologies,team_project,meetings,team_reflections,contacts]
+
+let element = document.getElementById("kangaroo_team_profiles");
+element.classList.add("active")
+document.getElementById("display_text").innerHTML = imports[0]
+
+
+
+
+
+    window.onclick = e => {
+    
+        try{
+        let element = document.getElementById(e.target.id).classList;
+       // console.log(e.target.id)
+
+        let index = 0
+        while (index< pages.length){
+           if (e.target.id == pages[index] && (e.target.id != "")){
+
+        class_list() 
+        element.add("active");
+        contents(e.target.id)
+        } index++
+    
+    }}
+        catch(err){
+        //   console.log("The Error is:"+ err)
+
+        }
+
+    }
+
+    //Add page contents
+    let contents =(target)=>{
+
+        let index = 0
+        
+        while (index< pages.length){
+            if (target===pages[index]){
+                document.getElementById("display_text").innerHTML = imports[index]
+                break
+            }
+        index ++
+        
+        }
+    }
+    
+    //removes active page high lighted
+    let class_list =()=>{
+        const actives = document.querySelectorAll(".active");
+    for (let i = 0; i< actives.length;i++){
+        actives[i].classList.remove("active")
+    }}
+
---
+++ text.js
@@ -0,0 +1,90 @@
+export let kangaroo_team_profiles = 
+"<h1>Kangaroo Team Profiles</h1>" + 
+"<h2>Summary text</h2>" +
+"<p>text here</p>"+
+
+"<h1>John Gude</h1>" + 
+"<p>The folllowing personality type profile tests were carried out</p>"+
+"<ol>"+
+"<li><a href='https://www.16personalities.com/'>16personalities: Myers-Briggs Type Indicator (MBTI)</a></li>"+
+"<li><a href='https://www.seemypersonality.com/'>seemy personality test</a></li>"+
+"<li><a href='https://www.123test.com/disc-personality-test/'>123 test disc personality test</a></li>"+
+"</ol>"+
+"<h4>16personalities: Myers-Briggs Type Indicator (MBTI)</h4>" +
+"<p>The results of this test were relatively accurate, it allows me to understand my view of the world against others. This in turn allows me to interact and see the social interactions I have viewed from a different person and adjust as required to accommodate the connection. The test will influence me in that I will need to accommodate others understanding, allowing other to come to their own conclusions rather than judging the conclusion early, that they make. This will allow me to work within a team more effectively. Understanding roles within a team is important, however working within a team in other role outside of my ‘comfort zone’ will allow better and deeper understanding of how the team can be better equipped for the future.</p>"+
+"<div><img src='./jg_mbrigs.PNG'></div>"+
+
+"<h4>Seemy personality test</h4>" +
+"<p>The results of this test, shown below in the provided extract, are in my opinion like horoscopes, mass application to many. However, with the various results the following could be extrapolated, the test will influence me in that I will need to accommodate others understanding, allowing other to come to their own conclusions rather than judging the conclusion early that they make. This will allow me to work within a team more effectively. Understanding roles within a team is important, however working within a team in other roles outside of my ‘comfort zone’ will allow better and deeper understanding of how the team can be better equipped for the future. I have learned that not all online tests are made equally.</p>"+
+"<div><img src='./jg_seemly.PNG'></div>"+
+
+"<h4>123 test disc personality test</h4>" +
+"<p>The results of this test were not very accurate representation of me, it allows me to understand my view of the world against others, but I believe the test is not accurate. Using tests such as these could be detrimental to you if you followed them explicitly. The test will influence me in that I will need to accommodate others understanding, allowing other to come to their own conclusions rather than judging the conclusion early that they make. This will allow me to work within a team more effectively. Understanding roles within a team is important, however working within a team in other roles outside of my ‘comfort zone’ will allow better and deeper understanding of how the team can be better equipped for the future.I have learned that not all online tests are made equally.</p>"+
+"<div><img src='./jg_disk.PNG'></div>";
+
+
+export let it_interests = 
+
+"<h1>IT Interests</h1>" + 
+"<p>text here</p>"
+
+
+export let career_profiling = 
+
+"<h1>Ideal career Career Position</h1>" + 
+"<p>My ideal role would be Chief Digital Officer and follows on from my current role of Chief Technical Engineer ANZ. This role is the next evolutionary step in my career, having worked within this field for 25+ years my knowledge and understanding across multiple sectors at a global level is consummate to this role. This position provides me with the level of influence to develop global frameworks rather than national or country level frameworks. It allows access to additional resources and skills to enable cohesive ideas and technology solutions to come together to solve complex engineering problems. This solving of complex problems is one of the driving forces of choosing this career path.</p>"+
+"<p>The skills and qualifications in this role have more to do with experience and exposure to the type of work involved, soft skills in how I present, articulate arguments and leadership in direction for technology solutions. It is however better to have qualifications which support the experience, and therefore why I am trying to achieve a degree in IT.</P>"+
+"<p>In summary the skills and qualifications required may consist of:</P>"+
+"<ol>"+
+"<li>Background in IT and/or Engineering (20+years)</li>"+
+"<li>People leader at international and global levels</li>"+
+"<li>Deep understanding of financial market strategy affecting business decision making</li>"+
+"<li>Deep understanding of IT market movements and strategy</li>"+
+"<li>Able to present complex ideas at board/executive level</li>"+
+"<li>Understanding of security systems and legacy evolutionary measures</li>"+
+"<li>Qualifications: Degree/MBA/PHD level IT, Engineering and Project Management or eqv exp."+
+"</ol>"+
+"<p>The plan to achieve this is to complete a IT degree and register for professional memberships to compliment my current role (Chief Technical Engineer:Digital, Systems,Signalling) and transition through promotion into the CDO role.</p>";
+
+export let it_technologies =
+
+"<h1>IT Technologies</h1>" + 
+"<h2>Technology Name</h2>" +
+"<h4>Introduction</h4>" +
+"<p>text </p>"
+
+export let team_project =
+
+"<h1>Team Project</h1>" + 
+"<h2>Project Name</h2>" +
+"<h4>Introduction</h4>" +
+"<p>text </p>"
+
+
+export let meetings =
+
+"<h1>Meetings</h1>" + 
+"<h2>text</h2>" +
+"<h4>text</h4>" +
+"<p>text </p>"
+
+
+
+		
+export let team_reflections =
+
+"<h1>Team Reflections</h1>" + 
+"<h2>text</h2>" +
+"<h4>text</h4>" +
+"<p>text </p>"
+
+
+		
+		
+export let contacts =
+
+"<h1>Contacts</h1>" + 
+"<h2>Team Contact information</h2>" +
+"<p>add table here</p>"+
+"<p>John Gude <a href='mailto:S3973475@student.rmit.edu.au'>S3973475@student.rmit.edu.au</a></p>"
+
